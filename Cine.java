fc0ff89beb2a2586147ec6f7042a4ec7bcb29e75
UnaxGainzarain <unaxarbizu1@gmail.com>
2026-01-02 14:24:48 +0100

    Añadida sincroniacion wait nitify para gestión de colas

--- Cine.java
+++ Cine.java
@@ -1,6 +1,24 @@
 package ejercicio3Sincronizacion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cine {
-	private int aforo;
-    public Cine(int aforo) { this.aforo = aforo; }
+	private List<Cliente> cola = new ArrayList<>(); // Cola única (V1)
+
+    // El cliente llama a esto
+    public synchronized void ponerseEnCola(Cliente c) { // 
+        cola.add(c);
+        System.out.println("Cliente " + c.getId() + " en cola.");
+        notify(); // Avisa a una taquilla dormida 
+    }
+
+    // La taquilla llama a esto
+    public synchronized Cliente atenderCliente() throws InterruptedException {
+        while (cola.isEmpty()) {
+            wait(); // Espera a que llegue alguien 
+        }
+        return cola.remove(0);
+    }
+    
 }
