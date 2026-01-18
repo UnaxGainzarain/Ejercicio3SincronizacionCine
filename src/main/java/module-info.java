92664f42cbd599ea1c8b87836fac5d951a59be7b
UnaxGainzarain <unaxarbizu1@gmail.com>
2026-01-18 18:44:00 +0100

    Subida carpeta adecuada a git

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * A simple jigsaw module
+ */
+module SicronizacionHilos {
+  requires org.slf4j;
+  requires ch.qos.logback.classic;
+  //equires static lombok;
+
+  exports ejercicio1;
+  }
